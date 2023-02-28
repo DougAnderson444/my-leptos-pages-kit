c9d1c5408bd72e1faf259979bc552179ebd66b5d
Doug A <douganderson444@gmail.com>
2023-02-28 10:01:14 -0400

    configure base path for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+// import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static'; // if no server side code, works for both github and cloudflare
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+export const serverPath = '/my-leptos-pages-kit';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,8 +10,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	},
+		adapter: adapter({
+			pages: 'docs', // github pages
+			assets: 'docs', // github pages
+			fallback: 'index.html' // for static site prerendering
+		}),
+		paths: {
+			base: serverPath // process.env.NODE_ENV === 'development' || process.argv.includes('dev') ? '' : serverPath
+		}
+	}
 	// preprocess: vitePreprocess() // will preprocess our tailwindcss
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import rust from '@wasm-tool/rollup-plugin-rust';
+import { serverPath } from './svelte.config.js';
 
 export default defineConfig({
-	plugins: [rust(), sveltekit()],
+	plugins: [
+		rust({
+			serverPath: serverPath + '/' // for github pages
+		}),
+		sveltekit()
+	],
 	optimizeDeps: { exclude: ['./rust/*'] }, // doesnt exclude ryst/Cargo.toml from ssrTrnsformation
 	ssr: { external: ['./rust/*'] }, // doesnt exclude ryst/Cargo.toml from ssrTrnsformation
 	server: { fs: { strict: false } }
